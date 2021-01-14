7399fd248e029e7dfe7d94e41860b31a510f9653
Kara F5 <karafan88@gmail.com>
2021-01-14 14:14:16 -0800

    making the area for the enemies

---
+++ Agar.io P3-4/src/Driver.java
@@ -0,0 +1,152 @@
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+
+public class Driver extends JPanel implements MouseListener, ActionListener{
+	
+	//Create an array of enemies
+	Enemy[] enemies= new Enemy[100];
+	
+	
+	public void paint(Graphics g) {
+		for(int i=0; i< enemies.length-1;i++) {
+		for(int j=i+1; j<enemies.length-1;i++) {
+			if(enemies[i].collided(enemies[j])) {
+			//smaller gets absorbed
+					//teleport the small one out of the screen
+					//enemies[i].getradius 
+					double r= enemies[i].getW1();//creating the variables for the radius
+					double r1= enemies[i].getW1();
+					
+					
+					
+					
+					double area1= (int)r*r1*(Math.PI);//creating the variables for the area using the circle area formula
+					double area2=(int)r*r1*(Math.PI);
+					double newArea= area1+area2;
+					
+					int W2=(int)(Math.sqrt(newArea/Math.PI));
+					int speed2= 80/W2;
+					if(r>r1) {
+						enemies[i]= setX(1000);
+						enemies[j]= setW1(W2);
+						enemies[i]= setSpeed(speed2);
+					}else if(r<r1) {
+						enemies[j]= setX(1000);
+						enemies[i]= setW1(W2);
+						enemies[j]= setSpeed(speed2);
+					}
+					
+					//new area creates a double, then cast to int
+					//get the radius with get width, check which one you have a bigger radius and move the small one out of the screen
+				}
+			}
+		}
+		
+		super.paintComponent(g); //proper redrawing of the entire screen
+		for(Enemy tempVar: enemies) {
+			
+		}
+		//remove after checking that your program is runnable
+		//g.fillRect(30, 30, 50, 50);
+			//call the Enemy objects to paint themselves
+		//for each - enhanced loop
+		for(Enemy tempVar: enemies) {//for every enemy en in enemies invoke the paint method 
+			tempVar.paint(g); 
+		}
+		 
+	}
+	
+ 
+	private Enemy setSpeed(int speed2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	private Enemy setW1(int w2) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	private Enemy setX(int i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public Driver(){
+		JFrame frame = new JFrame("Agar.io");
+		frame.setSize(800,600);
+		frame.add(this);
+		
+		/* add 100  Enemies */
+		//visit each position in the enemies array
+		for(int i=0; i< enemies.length; i++) {
+			enemies[i]= new Enemy();
+		}
+	
+		Timer t = new Timer(16, this); //chose swing library for import
+		t.start();
+		
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+	
+	
+	
+	
+	public static void main(String[] arg) {
+		Driver d = new Driver();
+	}
+	
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		System.out.println("here");
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		repaint(); //Timer will invoke this method which then refreshes the screen
+				   // for the "animation"
+	}
+	
+}
---
+++ Agar.io P3-4/src/Enemy.java
@@ -0,0 +1,116 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+public class Enemy {
+	//instance
+	private double mass;
+	private int w1,w2;
+	private int  x, y;
+	private int speed;
+	private int speedX, speedY;
+	private Color color;
+	private int r;
+	private int radius;
+	
+	//initial values
+	public Enemy() {
+		//x=200;
+		//y=200;
+		x= (int)(Math.random()*800);
+		//x1= (int)(Math.random()*200);
+		
+		y=(int)(Math.random()*600); 
+		//y1=(int)(Math.random()*200);
+		w1= (int)( Math.random()*(80-20+1)+20);
+		speedY= (int)(Math.random()*(3)+1);
+		speedX= (int)(Math.random()*(3)+1);
+		speedX=(int)(Math.random()*(2)-3);
+		speedY= (int)(Math.random()*(2)-3);
+		
+		speedX= 80/w1;
+		speedY= 80/w1;
+		
+		if(Math.random()>0.5){ 
+			speedX*=-1; 
+		}
+		if(Math.random()>0.5){ 
+			speedY*=-1; 
+		}
+		
+		
+		
+		
+		 
+		Color myColor= new Color(30,50,40);
+		int red= (int)(Math.random()*256);//make the enemies have red, green, or blue randomly
+		int green= (int)(Math.random()*256);
+		int blue= (int)(Math.random()*256);
+		color= new Color(red, green, blue);
+		//randomize the position of every Enemy object to be in the 800*600 frame(width*height)
+		//randomize width[20 80]
+		//randomize speed to between [-3 3] (non zero)
+		//width=50;
+	
+
+	}
+	public int getW1() {
+		return w1/2; //getter for the radius
+	}
+	public void setW1(int newW1) {
+		w1=newW1;//setter for the width
+	}
+	public void setX(int paramX) {
+		x=paramX; //setter for the X coordinate
+	}
+	
+	private int random(int width2) {
+		// TODO Auto-generated method stub
+		return 0;
+		//if(( Math.sqrt((y2 - y) * (y2 - y) + (x2 - x) * (x2 - x));)
+	}
+
+	public boolean collided(Enemy en2) {
+		
+		//obj1(the object you are asking)
+		//go from the corner of the oval to the center
+		int x1= this.x +this.w1/2;
+		//which ever object this method is calling, is asking this object
+		int y1= this.y +this.w1/2;
+		//obj2 coordinate
+		int x2= en2.x+en2.w1/2;
+		int y2= en2.x +en2.w1/2;
+		double d= Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2));//Math.pow(base, expo)
+		if(d<w1/2+w2/2) {
+			return true;
+		}
+		    return false;
+	}//find the area, then 
+	
+	
+	
+	public void paint(Graphics g) {
+		
+		update(); //call method that helps with updating vars
+		
+		
+		//call fillOval here
+		g.setColor(color);
+		g.fillOval(x,  y,  w1,  w1);
+   
+	}
+		
+		
+	/* anything that updates the variables of this object */
+	public void update() {
+		//update x and y based on SpeedX and speedY
+		x+=speedX;
+		y+= speedY;
+	}
+	public double getRadius() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
